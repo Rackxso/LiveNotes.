6622a0a4f62fe4da0f68635ce1f0afe657337160
Rackxso <oscarurenas04@gmail.com>
2026-05-22 11:50:35 +0200

    Work in progress out

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  workInProgress: true,
+  workInProgress: false,
   apiUrl: 'https://backendlivenotesmongodb.onrender.com/api',
   posthogKey: 'phc_pKUdWL3jmtw5rtE532ZNWs64GnY7YM8GE9Sj9frgJk4H',
   posthogHost: 'https://eu.i.posthog.com'
